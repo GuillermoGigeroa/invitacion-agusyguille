570330ff06aaba8e72ea4485dba43702414d4e68
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2024-06-05 15:14:01 -0300

    Se agrega nueva versión con cambios

---
+++ src/app/components/countdown-show/countdown-show.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown-show',
+  templateUrl: './countdown-show.component.html',
+  styleUrls: ['./countdown-show.component.scss']
+})
+export class CountdownShowComponent {
+
+}
--- src/app/pages/countdown/countdown.module.ts
+++ src/app/pages/countdown/countdown.module.ts
@@ -4,6 +4,7 @@ import { CountdownRoutingModule } from './countdown-routing.module';
 import { CountdownComponent } from './countdown.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CountdownShowComponent } from 'src/app/components/countdown-show/countdown-show.component';
 
 
 @NgModule({
@@ -15,9 +16,11 @@ import { SharedModule } from 'src/app/shared/shared.module';
   ],
   declarations: [
     CountdownComponent,
+    CountdownShowComponent,
   ],
   exports: [
     CountdownComponent,
+    CountdownShowComponent,
   ]
 })
 export class CountdownModule { }
