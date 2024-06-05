83d6b589125a8aa317970200475b8138da01e816
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2024-06-05 01:01:40 -0300

    Nueva versión

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { UtilsService } from './shared/utils.service';
 })
 export class AppComponent implements OnInit {
   readonly notMobileMessage = 'Para una mejor experiencia, por favor, ingresá desde un dispositivo móvil.';
+  audio: HTMLAudioElement = new Audio('assets/sounds/love-of-my-life.mp3');
+  music = false;
+
   constructor(private utilsService: UtilsService) {}
 
   ngOnInit(): void {
@@ -15,6 +18,16 @@ export class AppComponent implements OnInit {
     if (!isMobile) {
       console.log(`${this.notMobileMessage}`);
       alert(`${this.notMobileMessage}`);
+    }
+  }
+
+  playMusic() {
+    if (!this.music) {
+      this.audio.play();
+      this.music = true;
+    } else {
+      this.audio.pause();
+      this.music = false;
     }
   }
 }
