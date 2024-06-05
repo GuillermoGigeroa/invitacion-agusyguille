e496a7d9bf44d126f85035a254079ba855004d05
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2024-06-05 12:46:21 -0300

    Se agrega nueva versión con cambios

---
+++ update.py
@@ -0,0 +1,27 @@
+def execute(command):
+    import os
+    os.system(command)
+
+def clear(): 
+    execute('clear | cls')
+
+def build():
+    execute('npm run build')
+
+def addAll():
+    execute('git add .')
+
+def commit():
+    execute('git ci -m "Se agrega nueva versión con cambios"')
+
+def push():
+    execute('git push')
+
+def __init__():
+    clear()
+    build()
+    addAll()
+    commit()
+    push()
+
+__init__()
